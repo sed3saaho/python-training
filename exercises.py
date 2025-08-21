1a9df88c13780399b08e2019dbfc8a2811990352
Sedly <sedly.keith@student.moringaschool.com>
2025-08-21 20:27:46 +0300

    elif

--- exercises.py
+++ exercises.py
@@ -342,7 +342,17 @@ elif age < 18:
 else:
     price = 40
 print(f"Your admission cost is ${price}.")
-
+#You can use as many elif blocks in your code as you like.
+age = 65
+if age < 4:
+    price = 0
+elif age < 8:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+print(f"Your admission cost is ${price}")
 
 
 
