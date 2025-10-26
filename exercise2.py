4b7645a51a42c192d314d53ab541c2980590b8c0
Sedly <sedly.keith@student.moringaschool.com>
2025-10-26 21:43:55 +0300

    updating the exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,10 @@
+#Dictionaries
+alien_1 = {'color': 'green', 'points': 5}
+print(alien_1['color'])
+print(alien_1['points'])
+
+person_1 = {'name':'Sedly Okolla', 'age':'26', 'nickname':'Saaho', 'wife':'Loise Okolla'}
+print(person_1['name'])
+print(person_1['age'])
+print(person_1['nickname'])
+print(person_1['wife'])
